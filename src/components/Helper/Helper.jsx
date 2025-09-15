04e509819ccd9de453c1a1d26a59d5ebf2d150bf
Phuc Hau <phuchau.fit@gmail.com>
2025-09-15 10:16:32 +0700

    test

--- src/components/Helper/Helper.jsx
+++ src/components/Helper/Helper.jsx
@@ -3,12 +3,12 @@ import "./Helper.css"
 
 const Helper = () => {
   return (
-    <div className='adver'>
-        <div className="adver__1">
+    <div>
+        <div>
             <img src="https://pos.nvncdn.com/14f951-12134/art/artCT/20230723_Vne80hqk.jpg" alt="" />
         </div>
 
-        <div className="adver__2">
+        <div>
             <img src="https://pos.nvncdn.com/14f951-12134/art/artCT/20230723_Vne80hqk.jpg" alt="" />
             <img src="https://pos.nvncdn.com/14f951-12134/art/artCT/20230723_Vne80hqk.jpg" alt="" />
         </div>
