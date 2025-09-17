7d9be02d786718c6bc0372c4134b041e4a5464c6
Phuc Hau <phuchau.fit@gmail.com>
2025-09-17 20:24:04 +0700

    chuan bi dep

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,18 +3,34 @@ import adminRouter from "./routes/admin.router.js"
 import {connectDB} from "./config/db.js"
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from "path"
 
 dotenv.config()
+
+const __dirname = path.resolve()
+
 const app = express()
 
 //middleware
 app.use(express.json())
-app.use(cors({origin: "http://localhost:5173"}))
+
+if (process.env.NODE_ENV !== 'production'){
+    app.use(cors({origin: "http://localhost:5173"}))
+}
+
 
 connectDB()
 const port = process.env.PORT || 5001
 
 app.use("/admin", adminRouter)
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
+
 
 
 app.listen(port, () => {
